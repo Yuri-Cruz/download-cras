9416f605286c245f5f8ded0c029d062cedf6f698
Yuri-Cruz <yuri.cruz@bandtec.com.br>
2021-05-26 18:53:20 -0300

    paz

---
+++ scriptBd.sql
@@ -0,0 +1,34 @@
+create database crasdefault;
+
+use crasdefault;
+
+create table Maquinas(
+	hostName varchar(255) primary key,
+                sistemaoperacional varchar(255),
+	processadorInfo varchar(255),
+                qtdMemoria varchar(255),
+                qtdParticoes int,
+	latidude varchar(255), 
+	longitude varchar(255)
+);
+
+create table logs (
+	momentoCaptura datetime,
+	usoCpu varchar(255),
+	usoMemoria varchar(255),
+	tempoUso varchar(255),
+                fkMaquina varchar(255), foreign key(fkMaquina) references Maquinas(hostName),
+	primary key (fkMaquina, momentoCaptura)
+);
+
+create table discos(
+	id int  NOT NULL AUTO_INCREMENT,
+	momentoCaptura datetime,
+	pontoDeMontagem varchar(255),
+	espacoTotal double,
+	espacoDisponivel double,
+	fkMaquina varchar(255), foreign key(fkMaquina) references Maquinas(hostName),
+	primary key (fkMaquina, momentoCaptura),
+	key id (id)
+
+) AUTO_INCREMENT=1; 
